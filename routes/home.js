08030f66e94528c7c829f5364d3392786e7b5b0e
Владислав Кореньков <vladnfs3@gmail.com>
2021-11-07 20:39:23 +1000

    response in home GET misspell fixed

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json(
-    "You're on Shortify. To start, send the POST request with longUrl" +
+    "You're on Shortify. To start, send the POST request with longUrl " +
       "parameter provided in body (JSON). You'll get response with your shorten link."
   );
 });
