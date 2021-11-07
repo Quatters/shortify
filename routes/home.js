6cc823e2e349853a7d72f17c8a4a6aea7101f903
Владислав Кореньков <vladnfs3@gmail.com>
2021-11-07 20:55:26 +1000

    address for POST in home route added

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.json(
     "You're on Shortify. To start, send the POST request with longUrl " +
-      "parameter provided in body (JSON). You'll get response with your shorten link."
+      "parameter provided in body (JSON) to /api/url/shortify. You'll get response with your shorten link."
   );
 });
 
